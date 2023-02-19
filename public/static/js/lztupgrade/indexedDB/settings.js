2ca941f877884955414c70157b527a782de446ba
Toil <toil.official@yandex.com>
2023-02-20 02:07:11 +0300

    added indexeddb for settings

---
+++ public/static/js/lztupgrade/indexedDB/settings.js
@@ -0,0 +1,44 @@
+/**
+ *
+ * @author Toil
+ * @license MIT
+ * @copyright Toil
+ * @created 2023-02-17
+ *
+ * @description LZTSettingsDB class implementation
+ *
+ */
+
+class LZTSettingsDB extends LZTUpgradeDB {
+  /**
+   *
+   *  @constructor
+   *  @param {string} name - name of the database
+   *  @param {string} objectKey - name of the future indexedDB object
+   *  @param {number} version - version of the database
+   */
+
+  constructor(name = 'LZTUpSettings', objectKey = 'settings', version = 1) {
+    super(
+      name,
+      objectKey,
+      version,
+      [ // indexes
+        'checkUpdatesOnLoad',
+      ],
+      { // indexesWithTypes
+        'checkUpdatesOnLoad': 'number',
+      },
+      { // defaultData
+        key: objectKey,
+        checkUpdatesOnLoad: 0,
+      }
+    );
+  };
+
+  async update({
+    checkUpdatesOnLoad,
+  }) {
+    super.update(...arguments);
+  }
+}
