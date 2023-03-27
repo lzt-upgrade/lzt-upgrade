ce50afda470f3b486c10aefa0c7e11d93800e47c
Toil <toil.official@yandex.com>
2023-03-28 01:44:53 +0300

    added docstring

--- server/models/user_sign.py
+++ server/models/user_sign.py
@@ -3,6 +3,14 @@ from pydantic import BaseModel
 
 
 class UserSign(BaseModel):
+    """Класс пользовательского значка
+
+    Args:
+        uid (int): ID пользовательского значка
+        userid (int): ID пользователя
+        signid (int): ID значка
+        created_at (int): Время создания
+    """
     uid: int
     userid: int
     signid: int
