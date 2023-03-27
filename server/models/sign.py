33898066a596a51afb15301401f3685605f41d9b
Toil <toil.official@yandex.com>
2023-03-28 00:58:08 +0300

    added docstring

--- server/models/sign.py
+++ server/models/sign.py
@@ -2,6 +2,14 @@ from pydantic import BaseModel
 
 
 class Sign(BaseModel):
+    """ Класс значка
+
+    Args:
+        uid (str): Уникальный ID значка
+        system_name (str): Отображение в API
+        name (str): Отображение в интерфейсе
+        image_link (str): Ссылка на изображение значка
+    """
     uid: int
     system_name: str
     name: str
