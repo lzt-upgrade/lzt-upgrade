36ce5ddb389baf4de603ac0ee75ddc1b7e4ab695
Toil <toil.official@yandex.com>
2023-03-28 01:59:38 +0300

    rewrited with types

--- server/sql/logos/controller.py
+++ server/sql/logos/controller.py
@@ -1,27 +1,28 @@
+from models.logo import Logo
 from sql.logos.service import DbLogosService
 
 
 class DbLogosController:
-    async def init(self):
+    async def init(self) -> bool:
         """Создаёт таблицу в базе данных, если она не существует"""
         return await DbLogosService().init_table()
 
-    async def add_logo(self, name: str, file: str, author: str, author_userid: str = None, accent_color: str = None, text_color: str = None, active: bool = None):
+    async def add_logo(self, logo: Logo) -> bool:
         """Добавляет лого в базу данных"""
-        return await DbLogosService().add_logo(name, file, author, author_userid, accent_color, text_color, active)
+        return await DbLogosService().add_logo(logo)
 
-    async def get_all(self):
+    async def get_all(self) -> list[Logo]|bool:
         """Возвращает все лого из базы данных"""
         return await DbLogosService().get()
 
-    async def get_by_target(self, target: int = 1):
+    async def get_by_target(self, target: int = 1) -> list[Logo]|bool:
         """Возвращает лого из базы данных по цели (1 - форум, 2 - маркет)"""
         return await DbLogosService().get(target = target)
 
-    async def get_by_author(self, author: str):
+    async def get_by_author(self, author: str) -> list[Logo]|bool:
         """Возвращает лого из базы данных по имени автора"""
         return await DbLogosService().get(author = author)
 
-    async def get_by_author_userid(self, author_userid: str):
+    async def get_by_author_userid(self, author_userid: int) -> list[Logo]|bool:
         """Возвращает лого из базы данных по айди автора"""
         return await DbLogosService().get(author_userid = author_userid)
--- server/sql/logos/service.py
+++ server/sql/logos/service.py
@@ -1,102 +1,97 @@
 import logging
-import time
+import aiomysql
 
+from models.logo import Logo
 from sql.db import DefaultConnector
 
 
 class DbLogosService:
     def __init__(self) -> None:
-        self.database_sc = 'lzt_logos' # Название таблицы в базе данных (для логов)
-        self.log = logging.getLogger('server')
+        self.database_sc: str = 'lzt_logos' # Название таблицы в базе данных (для логов)
+        self.log = logging.getLogger('mysql')
 
-    async def init_table(self):
+    async def init_table(self) -> bool:
         """Создает таблицу в базе данных для правильной работы"""
         db = await DefaultConnector().connect()
-        try:
-            async with db.cursor() as cursor:
-                await cursor.execute("""CREATE TABLE IF NOT EXISTS `lzt_logos` (
-                    `uid` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-                    `name` VARCHAR(512) NOT NULL,
-                    `preview` VARCHAR(512) NOT NULL,
-                    `target` INT NOT NULL DEFAULT 1,
-                    `css` VARCHAR(2048) NOT NULL,
-                    `author` VARCHAR(512) NOT NULL DEFAULT 'Lolzteam',
-                    `author_userid` VARCHAR(128) DEFAULT NULL,
-                    `active` TINYINT(1) NOT NULL DEFAULT 0,
-                    `created_at` INT NOT NULL DEFAULT 0
-                )""")
-                return True
-        except AttributeError as err:
-            self.log.error(f'Failled connection to database: {err}')
-            return None
-        except Exception as err:
-            self.log.exception(f'Failed to init table in database ({self.database_sc}): {err}')
-            return None
-        finally:
-            db.close() if db else None
+        if type(db) is aiomysql.Connection:
+            try:
+                async with db.cursor() as cursor:
+                    await cursor.execute("""CREATE TABLE IF NOT EXISTS `lzt_logos` (
+                        `uid` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+                        `name` VARCHAR(512) NOT NULL,
+                        `preview` VARCHAR(512) NOT NULL,
+                        `target` INT NOT NULL DEFAULT 1,
+                        `css` VARCHAR(2048) NOT NULL,
+                        `author` VARCHAR(512) NOT NULL DEFAULT 'Lolzteam',
+                        `author_userid` VARCHAR(128) DEFAULT NULL,
+                        `active` TINYINT(1) NOT NULL DEFAULT 0,
+                        `created_at` INT NOT NULL DEFAULT (UNIX_TIMESTAMP())
+                    )""")
+                    return True
+            except Exception as err:
+                self.log.exception(f'Failed to init table in database ({self.database_sc}): {err}')
+                return False
+            finally:
+                db.close()
+        self.log.error(f'Failed connection to database')
+        return False
 
-    async def add_logo(self, name: str, preview: str, target: int, css: str, author: str, author_userid: str, active: bool):
+    async def add_logo(self, logo: Logo) -> bool:
         """Добавляет лого в базу данных
 
         Args:
-            name: имя лого
-            preview: прямая ссылка на лого (превьюшка в меню)
-            target: цель лого (1 - форум, 2 - маркет)
-            css: применяемый css
-            author: имя автора (если авторов больше одного, то через запятую)
-            author_userid: id автора (если авторов больше одного, то через запятую)
-            active: активен ли логотип
+            logo (Logo): объект логотипа
 
         Returns:
             True: Если удалось добавить лого
-            None: Если произошла ошибка
+            False: Если произошла ошибка
         """
         db = await DefaultConnector().connect()
-        try:
-            async with db.cursor() as cursor:
-                timestamp = int(time.time())
-                await cursor.execute('INSERT INTO `lzt_logos` (`name`, `preview`, `target`, `css`, `author`, `author_userid`, `active`, `created_at`) VALUES (%s)', (name, preview, target, css, author, author_userid, active, timestamp))
-                await db.commit()
-                return True
-        except AttributeError as err:
-            self.log.error(f'Failled connection to database: {err}')
-            return None
-        except Exception as err:
-            self.log.exception(f'Failed to add a logo to the database ({self.database_sc}): {err}')
-            return None
-        finally:
-            db.close() if db else None
+        if type(db) is aiomysql.Connection:
+            try:
+                async with db.cursor() as cursor:
+                    await cursor.execute('INSERT INTO `lzt_logos` (`name`, `preview`, `target`, `css`, `author`, `author_userid`, `active`, `created_at`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)',
+                                        (logo.name, logo.preview, logo.target, logo.css, logo.author, logo.author_userid, logo.active, logo.created_at))
+                    await db.commit()
+                    return True
+            except Exception as err:
+                self.log.exception(f'Failed to add a logo to the database ({self.database_sc}): {err}')
+                return False
+            finally:
+                db.close()
+        self.log.error(f'Failed connection to database')
+        return False
 
-    async def get(self, target: int = None, author: str = None, author_userid: str = None):
+    async def get(self, target: int|None = None, author: str|None = None, author_userid: int|None = None) -> list[Logo]|bool:
         """Возвращает лого из базы данных
 
         Args:
-            target: цель лого (1 - форум, 2 - маркет)
-            author: имя автора (default: None)
-            author_userid: id автора (default: None)
+            target (int|None): цель лого (1 - форум, 2 - маркет)
+            author (str|None): имя автора
+            author_userid (int|None): id автора
 
         Returns:
             data: Если удалось получить данные
-            None: Если произошла ошибка
+            False: Если произошла ошибка
         """
         db = await DefaultConnector().connect()
-        try:
-            async with db.cursor() as cursor:
-                if target and type(target) == int:
-                    await cursor.execute('SELECT * FROM `lzt_logos` WHERE `target` = %s', (target, ))
-                elif author and type(author) == str:
-                    await cursor.execute('SELECT * FROM `lzt_logos` WHERE `author` = %s', (author, ))
-                elif author_userid and type(author_userid) == int:
-                    await cursor.execute('SELECT * FROM `lzt_logos` WHERE `author_userid` = %s', (author_userid, ))
-                else:
-                    await cursor.execute('SELECT * FROM `lzt_logos`')
-                result = await cursor.fetchall()
-                return result
-        except AttributeError as err:
-            self.log.error(f'Failled connection to database: {err}')
-            return None
-        except Exception as err:
-            self.log.exception(f'Failed to get theme (author: {author}, author_userid: {author_userid}) from database ({self.database_sc}): {err}')
-            return None
-        finally:
-            db.close() if db else None
+        if type(db) is aiomysql.Connection:
+            try:
+                async with db.cursor() as cursor:
+                    if target:
+                        await cursor.execute('SELECT * FROM `lzt_logos` WHERE `target` = %s', (target, ))
+                    elif author:
+                        await cursor.execute('SELECT * FROM `lzt_logos` WHERE `author` = %s', (author, ))
+                    elif author_userid:
+                        await cursor.execute('SELECT * FROM `lzt_logos` WHERE `author_userid` = %s', (author_userid, ))
+                    else:
+                        await cursor.execute('SELECT * FROM `lzt_logos`')
+                    result = await cursor.fetchall()
+                    return result
+            except Exception as err:
+                self.log.exception(f'Failed to get logos (author: {author}, author_userid: {author_userid}, target: {target}) from database ({self.database_sc}): {err}')
+                return False
+            finally:
+                db.close()
+        self.log.error(f'Failed connection to database')
+        return False
