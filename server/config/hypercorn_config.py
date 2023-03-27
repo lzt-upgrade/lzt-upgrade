34e9d5cbe6f96a85ad641b2c1a138326172939c4
Toil <toil.official@yandex.com>
2023-03-27 13:04:23 +0300

    renamed LOGGING category of config to logging

--- server/config/hypercorn_config.py
+++ server/config/hypercorn_config.py
@@ -7,7 +7,7 @@ from config.load import load_env, load_cfg
 load_env()
 env_port = os.environ.get('PORT')
 PORT = int(env_port) if env_port else 5000
-logs = load_cfg()['LOGGING']
+logs = load_cfg()['logging']
 
 bind = f'0.0.0.0:{PORT}'
 use_reloader = True
