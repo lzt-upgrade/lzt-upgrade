2e6aad41da0285ac75503176f28316f865da0cc9
Toil <toil.official@yandex.com>
2023-03-28 01:46:43 +0300

    added docstring

--- server/models/logo.py
+++ server/models/logo.py
@@ -1,7 +1,21 @@
+import time
 from pydantic import BaseModel
 
 
 class Logo(BaseModel):
+    """Класс логотипа
+
+    Args:
+        uid (int): ID логотипа
+        name (str): имя логотипа
+        preview (str): прямая ссылка на лого (превьюшка в меню)
+        target (int): цель лого (1 - форум, 2 - маркет)
+        css (str): применяемый css
+        author (str): имя автора (если авторов больше одного, то через запятую)
+        author_userid (int|None): id автора (если авторов больше одного, то через запятую)
+        active (int): активен ли логотип
+        created_at (int): время создания
+    """
     uid: int
     name: str
     preview: str
@@ -10,4 +24,4 @@ class Logo(BaseModel):
     author: str = 'Lolzteam'
     author_userid: int|None = None
     active: int = 0 # 0 - inactive, 1 - active
-    created_at: int = 0
+    created_at: int = int(time.time())
