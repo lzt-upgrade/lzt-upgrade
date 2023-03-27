b775f1095b6a9409814460de99072adc64eac97b
Toil <toil.official@yandex.com>
2023-03-28 02:10:47 +0300

    rewrited with types

--- server/sql/themes/controller.py
+++ server/sql/themes/controller.py
@@ -1,23 +1,24 @@
+from models.theme import Theme
 from sql.themes.service import DbThemesService
 
 
 class DbThemesController:
-    async def init(self):
+    async def init(self) -> bool:
         """Создаёт таблицу в базе данных, если она не существует"""
         return await DbThemesService().init_table()
 
-    async def add_theme(self, name: str, file: str, author: str, author_userid: str = None, accent_color: str = None, text_color: str = None, active: bool = None):
+    async def add_theme(self, theme: Theme) -> bool:
         """Добавляет тему в базу данных"""
-        return await DbThemesService().add_theme(name, file, author, author_userid, accent_color, text_color, active)
+        return await DbThemesService().add_theme(theme)
 
-    async def get_all(self):
+    async def get_all(self) -> list[Theme]|bool:
         """Возвращает все темы из базы данных"""
         return await DbThemesService().get()
 
-    async def get_by_author(self, author: str):
+    async def get_by_author(self, author: str) -> list[Theme]|bool:
         """Возвращает темы из базы данных по имени автора"""
         return await DbThemesService().get(author = author)
 
-    async def get_by_author_userid(self, author_userid: str):
+    async def get_by_author_userid(self, author_userid: int) -> list[Theme]|bool:
         """Возвращает темы из базы данных по айди автора"""
         return await DbThemesService().get(author_userid = author_userid)
--- server/sql/themes/service.py
+++ server/sql/themes/service.py
@@ -1,41 +1,42 @@
 import logging
-import time
+import aiomysql
 
+from models.theme import Theme
 from sql.db import DefaultConnector
 
 
 class DbThemesService:
     def __init__(self) -> None:
-        self.database_sc = 'lzt_themes' # Название таблицы в базе данных (для логов)
-        self.log = logging.getLogger('server')
+        self.database_sc: str = 'lzt_themes' # Название таблицы в базе данных (для логов)
+        self.log = logging.getLogger('mysql')
 
-    async def init_table(self):
+    async def init_table(self) -> bool:
         """Создает таблицу в базе данных для правильной работы"""
         db = await DefaultConnector().connect()
-        try:
-            async with db.cursor() as cursor:
-                await cursor.execute("""CREATE TABLE IF NOT EXISTS `lzt_themes` (
-                    `uid` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-                    `name` VARCHAR(512) NOT NULL,
-                    `file` VARCHAR(512) NOT NULL,
-                    `author` VARCHAR(512) NOT NULL DEFAULT 'Lolzteam',
-                    `author_userid` VARCHAR(128) DEFAULT NULL,
-                    `accent_color` VARCHAR(32) NOT NULL DEFAULT '#343434',
-                    `text_color` VARCHAR(32) NOT NULL DEFAULT '#d6d6d6',
-                    `active` TINYINT(1) NOT NULL DEFAULT 0,
-                    `created_at` INT NOT NULL DEFAULT 0
-                )""")
-                return True
-        except AttributeError as err:
-            self.log.error(f'Failled connection to database: {err}')
-            return None
-        except Exception as err:
-            self.log.exception(f'Failed to init table in database ({self.database_sc}): {err}')
-            return None
-        finally:
-            db.close() if db else None
+        if type(db) is aiomysql.Connection:
+            try:
+                async with db.cursor() as cursor:
+                    await cursor.execute("""CREATE TABLE IF NOT EXISTS `lzt_themes` (
+                        `uid` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+                        `name` VARCHAR(512) NOT NULL,
+                        `file` VARCHAR(512) NOT NULL,
+                        `author` VARCHAR(512) NOT NULL DEFAULT 'Lolzteam',
+                        `author_userid` VARCHAR(128) DEFAULT NULL,
+                        `accent_color` VARCHAR(32) NOT NULL DEFAULT '#343434',
+                        `text_color` VARCHAR(32) NOT NULL DEFAULT '#d6d6d6',
+                        `active` TINYINT(1) NOT NULL DEFAULT 0,
+                        `created_at` INT NOT NULL DEFAULT (UNIX_TIMESTAMP())
+                    )""")
+                    return True
+            except Exception as err:
+                self.log.exception(f'Failed to init table in database ({self.database_sc}): {err}')
+                return False
+            finally:
+                db.close()
+        self.log.error(f'Failed connection to database')
+        return False
 
-    async def add_theme(self, name: str, file: str, author: str, author_userid: str, accent_color: str, text_color: str, active: bool):
+    async def add_theme(self, theme: Theme) -> bool:
         """Добавляет тему в базу данных
 
         Args:
@@ -49,51 +50,51 @@ class DbThemesService:
 
         Returns:
             True: Если удалось добавить тему
-            None: Если произошла ошибка
+            False: Если произошла ошибка
         """
         db = await DefaultConnector().connect()
-        try:
-            async with db.cursor() as cursor:
-                timestamp = int(time.time())
-                await cursor.execute('INSERT INTO `lzt_themes` (`name`, `file`, `author`, `author_userid`, `accent_color`, `text_color`, `active`, `created_at`) VALUES (%s)', (name, file, author, author_userid, accent_color, text_color, active, timestamp))
-                await db.commit()
-                return True
-        except AttributeError as err:
-            self.log.error(f'Failled connection to database: {err}')
-            return None
-        except Exception as err:
-            self.log.exception(f'Failed to add a theme to the database ({self.database_sc}): {err}')
-            return None
-        finally:
-            db.close() if db else None
+        if type(db) is aiomysql.Connection:
+            try:
+                async with db.cursor() as cursor:
+                    await cursor.execute('INSERT INTO `lzt_themes` (`name`, `file`, `author`, `author_userid`, `accent_color`, `text_color`, `active`, `created_at`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)',
+                                        (theme.name, theme.file, theme.author, theme.author_userid, theme.accent_color, theme.text_color, theme.active, theme.created_at))
+                    await db.commit()
+                    return True
+            except Exception as err:
+                self.log.exception(f'Failed to add a theme to the database ({self.database_sc}): {err}')
+                return False
+            finally:
+                db.close()
+        self.log.error(f'Failed connection to database')
+        return False
 
-    async def get(self, author: str = None, author_userid: str = None):
+    async def get(self, author: str|None = None, author_userid: int|None = None) -> list[Theme]|bool:
         """Возвращает темы из базы данных
 
         Args:
-            author: имя автора (default: None)
-            author_userid: id автора (default: None)
+            author (str|None): имя автора
+            author_userid (int|None): id автора
 
         Returns:
             data: Если удалось получить данные
-            None: Если произошла ошибка
+            False: Если произошла ошибка
         """
         db = await DefaultConnector().connect()
-        try:
-            async with db.cursor() as cursor:
-                if author and type(author) == str:
-                    await cursor.execute('SELECT * FROM `lzt_themes` WHERE `author` = %s', (author, ))
-                elif author_userid and type(author_userid) == int:
-                    await cursor.execute('SELECT * FROM `lzt_themes` WHERE `author_userid` = %s', (author_userid, ))
-                else:
-                    await cursor.execute('SELECT * FROM `lzt_themes`')
-                result = await cursor.fetchall()
-                return result
-        except AttributeError as err:
-            self.log.error(f'Failled connection to database: {err}')
-            return None
-        except Exception as err:
-            self.log.exception(f'Failed to get theme (author: {author}, author_userid: {author_userid}) from database ({self.database_sc}): {err}')
-            return None
-        finally:
-            db.close() if db else None
+        if type(db) is aiomysql.Connection:
+            try:
+                async with db.cursor() as cursor:
+                    if author and type(author) == str:
+                        await cursor.execute('SELECT * FROM `lzt_themes` WHERE `author` = %s', (author, ))
+                    elif author_userid and type(author_userid) == int:
+                        await cursor.execute('SELECT * FROM `lzt_themes` WHERE `author_userid` = %s', (author_userid, ))
+                    else:
+                        await cursor.execute('SELECT * FROM `lzt_themes`')
+                    result = await cursor.fetchall()
+                    return result
+            except Exception as err:
+                self.log.exception(f'Failed to get theme (author: {author}, author_userid: {author_userid}) from database ({self.database_sc}): {err}')
+                return False
+            finally:
+                db.close()
+        self.log.error(f'Failed connection to database')
+        return False
