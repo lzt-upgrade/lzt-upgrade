7e09a8ad6f6467e21c106cf43719b05a26f0589c
Toil <toil.official@yandex.com>
2023-03-27 13:07:49 +0300

    added tags

--- server/routes/__init__.py
+++ server/routes/__init__.py
@@ -3,6 +3,6 @@ from fastapi import APIRouter
 from routes.endpoints import themes, logos
 
 api_router = APIRouter()
-api_router.include_router(themes.router, prefix = '/api')
-api_router.include_router(logos.router, prefix = '/api')
+api_router.include_router(themes.router, prefix = '/api', tags = ['Themes'])
+api_router.include_router(logos.router, prefix = '/api', tags = ['Logos'])
 
