f8605675eaa217f096674aa60a1a7b6cf5d7735e
Toil <toil.official@yandex.com>
2023-03-27 13:07:29 +0300

    rewritted

--- server/logger/main.py
+++ server/logger/main.py
@@ -11,27 +11,19 @@ from rich.style import Style
 from config.load import load_cfg
 
 
-settings = load_cfg()['LOGGING']
-
-def check_log_exists(part):
-    for file in [f'./logs/server{part}.log', f'./logs/worker{part}.log']: # For future use
-        if os.path.isfile(file):
-            return True
+settings = load_cfg()
 
 def init_logging():
-    root_logger = logging.getLogger()
-    server_log = logging.getLogger('server')
-    worker_log = logging.getLogger('worker')
-    logs = [server_log, worker_log] # For future use
-    if settings['level'] in [10, 20, 30, 40, 50]:
-        for log in logs:
-            log.setLevel(settings['level'])
+    logger = logging.getLogger()
 
-    file_formatter = logging.Formatter(
-            '[{asctime}] [{levelname}] {module}-{name}: {message}', datefmt = '%Y-%m-%d %H:%M:%S', style = '{'
-    )
+    if not os.path.isdir('./logs'):
+        try:
+            os.mkdir('./logs')
+            logger.info('Creating log directory')
+        except OSError as err:
+            logger.error(f'Failed to create log directory: {err}')
 
-    if settings['rich']:
+    if settings['logging']['rich_formatter']:
         rich_console = rich.get_console()
         rich.reconfigure(tab_size = 4)
         # Theme from https://github.com/Cog-Creators/Red-DiscordBot/blob/V3/develop/redbot/logging.py
@@ -55,24 +47,22 @@ def init_logging():
         stdout_handler.setFormatter(rich_formatter)
     else:
         stdout_handler = logging.StreamHandler(sys.stdout)
-        stdout_handler.setFormatter(file_formatter)
-    root_logger.addHandler(stdout_handler)
+
+    log_handlers = [stdout_handler]
+    if settings['logging']['save']:
+        log_name = f'./logs/main{datetime.now().strftime("%Y%m%d")}.log'
+        filehandler = logging.FileHandler(log_name)
+        filehandler.setLevel(settings['logging']['level'])
+        log_handlers.append(filehandler)
+
+    logging.basicConfig(
+        level = settings['logging']['level'],
+        datefmt = '%Y-%m-%d %H:%M:%S',
+        format = '[{asctime}] [{levelname}] {name}: {message}',
+        style = '{',
+        handlers = log_handlers
+    )
+
     logging.captureWarnings(True)
 
-    if settings['save']:
-        if not os.path.isdir('./logs'):
-            try:
-                os.mkdir('./logs')
-                root_logger.info('Creating log directory')
-            except OSError:
-                root_logger.exception('Failed to create log directory')
-        part = f'_{datetime.now().strftime("%Y%m%d")}'
-        if check_log_exists(part) is True:
-            file_mode = 'a'
-        else:
-            file_mode = 'w'
-        
-        for log in logs:
-            handler = logging.FileHandler(filename = f'./logs/{log.name}{part}.log', encoding = 'utf-8', mode = file_mode)
-            handler.setFormatter(file_formatter)
-            log.addHandler(handler)
+    return True
