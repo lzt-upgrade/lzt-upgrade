3c253a744db53d04b249ba0a27c0637a087532df
Toil <toil.official@yandex.com>
2023-03-31 15:35:59 +0300

    get availabled signs

--- server/routes/__init__.py
+++ server/routes/__init__.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
 
-from routes.endpoints import themes, logos, users
+from routes.endpoints import themes, logos, users, signs
 
 api_router = APIRouter(prefix='/api')
 api_router.include_router(themes.router, tags = ['Themes'])
 api_router.include_router(logos.router, tags = ['Logos'])
-api_router.include_router(users.router, tags = ['Users'])
+api_router.include_router(users.router, tags = ['Users', 'Signs'])
+api_router.include_router(signs.router, tags = ['Signs'])
 
