87fcbdabbb71cbd41d02b654990f8347684945ab
unknown <toil.official@yandex.com>
2023-05-04 02:41:31 +0300

    removed server from repo

--- server/main.py
+++
@@ -1,8 +0,0 @@
-import asyncio
-from hypercorn.asyncio import serve
-
-from server import start, app
-
-if __name__ == '__main__':
-    config = asyncio.run(start())
-    asyncio.run(serve(app, config))
--- server/start.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-python main.py
-
-pause
