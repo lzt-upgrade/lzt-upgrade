246c705b4bcf3f2de0d94452ce97574c7b6c1a38
unknown <toil.official@yandex.com>
2023-05-04 02:53:28 +0300

    random utils

---
+++ src/utils/utils.js
@@ -0,0 +1,38 @@
+async function waitForElement(selector, timeout = 15000) {
+  const start = Date.now();
+
+  while (Date.now() - start < timeout) {
+    const el = $(selector);
+    if ((el && el.length) || Date.now() - start > timeout) {
+      return el;
+    }
+    await new Promise(resolve => setTimeout(resolve, 1000));
+  }
+
+  return null;
+}
+
+const sleep = m => new Promise(r => setTimeout(r, m))
+
+function updateTooltips() {
+  let $lztUpTooltips = $('#LZTUpTooltip.Tooltip');
+  return XenForo.Tooltip($lztUpTooltips);
+}
+
+function getNodeLinks() {
+  let latestsThreads = $('div.latestThreads');
+  const stickyThreads = $('div.stickyThreads');
+  latestsThreads = stickyThreads.is(':visible') ? $.merge(latestsThreads, stickyThreads) : latestsThreads;
+  return latestsThreads.find('div.discussionListItem--Wrapper');
+}
+
+function getContestsLinks() {
+  let links = getNodeLinks()
+  .find('a.listBlock.main')
+  .toArray()
+  .map(element => $(element).attr('href'));
+  return links;
+}
+
+
+export { waitForElement, sleep, updateTooltips, getNodeLinks, getContestsLinks };
