65d3965a9643cba5bf01c3e341d22c7a95531cf5
unknown <toil.official@yandex.com>
2023-06-02 00:52:02 +0300

    Rewrited onParticipateHandler for sync function

--- src/utils/handlers.js
+++ src/utils/handlers.js
@@ -1,39 +1,31 @@
-import { waitForElement, sleep } from 'Utils/utils';
+import { waitForElm, sleep } from 'Utils/utils';
 import { Logger } from 'Utils/logger';
 
-function onClickCategoryContestsHandler(callback) {
-  const giveaways = $('li.node.node766.forum.level-n');
-  $(giveaways).on('click', async () => {
-    const el = await waitForElement('div.pageNavLinkGroup');
-    if (el) callback();
-  });
+function onParticipateHandler(callback) {
+  return waitForElm('.LztContest--Participate')
+    .then(el => {
+      Logger.debug('onParticipateHandler: Participate button finded');
+      el.addEventListener('click', async () => {
+        Logger.debug('onParticipateHandler: click contest button');
+        if (!el.classList.contains('disabled')) {
+          Logger.debug('onParticipateHandler: waiting for alreadyParticipate button');
+          const elem = await waitForElm('span.alreadyParticipate');
+          if (!elem) {
+            Logger.debug('onParticipateHandler: no alreadyParticipate button');
+            return;
+          }
+
+          Logger.debug('onParticipateHandler: alreadyParticipate button finded');
+          await sleep(1000);
+          callback();
+        } else {
+          Logger.debug('onParticipateHandler: clicked on disabled contest button');
+        }
+      })
+    })
+    .catch(el => {
+      Logger.debug('onParticipateHandler: no contest button');
+    });
 }
 
-async function onParticipateHandler(callback) {
-  const el = await waitForElement('.LztContest--Participate');
-  if (!el) {
-    Logger.debug('onParticipateHandler: no contest button');
-    return;
-  }
-
-  $(el).on('click', async () => {
-    Logger.debug('onParticipateHandler: click contest button');
-    if (!$(el).hasClass('disabled')) {
-      Logger.debug('onParticipateHandler: waiting for alreadyParticipate button');
-      const el = await waitForElement('span.alreadyParticipate');
-      if (!el) {
-        Logger.debug('onParticipateHandler: no alreadyParticipate button');
-        return;
-      }
-
-      console.log(el)
-      Logger.debug('onParticipateHandler: alreadyParticipate button finded');
-      await sleep(1000);
-      callback();
-    } else {
-      Logger.debug('onParticipateHandler: clicked on disabled contest button');
-    }
-  })
-}
-
-export { onClickCategoryContestsHandler, onParticipateHandler };
+export { onParticipateHandler };
