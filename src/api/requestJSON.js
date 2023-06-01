4a3a2a04d0f912d0d33168564a28fbdefdf39826
unknown <toil.official@yandex.com>
2023-06-02 00:33:14 +0300

    Rewrited with async ajax without callbacks

--- src/api/requestJSON.js
+++ src/api/requestJSON.js
@@ -1,17 +1,15 @@
 import { Logger } from 'Utils/logger'
 
 async function requestJSON(endpoint, errText) {
-  return $.ajax({
-    url: endpoint,
-    dataType: 'json',
-    success: (value) => {
-      return value
-    },
-    error: (err) => {
-      Logger.log(`${errText}. Ошибка: ${err}`);
-      return false;
-    }
-  });
+  try {
+    return await $.ajax({
+      url: endpoint,
+      dataType: 'json'
+    });
+  } catch (err) {
+    Logger.log(`${errText}. Ошибка:`, err);
+    return false;
+  }
 }
 
 export { requestJSON };
