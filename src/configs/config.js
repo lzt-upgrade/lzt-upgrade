58162e5c6abbded9b5d6f19ade951774df2525f2
unknown <toil.official@yandex.com>
2023-06-02 00:35:32 +0300

    Added nodeSelectors var, extName in builded version changed Preview -> Development Build

--- src/configs/config.js
+++ src/configs/config.js
@@ -1,6 +1,9 @@
 const config = () => {
   return {
-    'extName': DEV_MODE ? '[DEV] LZT Upgrade' : 'LZT Upgrade Preview'
+    'extName': DEV_MODE ? '[DEV] LZT Upgrade' : 'LZT Upgrade Development Build',
+    'nodeSelectors': {
+      'contests': '.node766'
+    }
   }
 }
 
