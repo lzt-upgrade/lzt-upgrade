c64cc1b0b88824489f3beddf328fa9d71064fbea
unknown <toil.official@yandex.com>
2023-06-02 01:37:11 +0300

    Moved Utils/handlers -> Events/participate

---
+++ src/events/participate.js
@@ -0,0 +1,31 @@
+import { waitForElm, sleep } from 'Utils/utils';
+import { Logger } from 'Utils/logger';
+
+function onParticipateHandler(callback, sleepTime = 1000) {
+  return waitForElm('.LztContest--Participate')
+    .then(el => {
+      Logger.debug('onParticipateHandler: Participate button finded');
+      el.addEventListener('click', async () => {
+        Logger.debug('onParticipateHandler: click contest button');
+        if (!el.classList.contains('disabled')) {
+          Logger.debug('onParticipateHandler: waiting for alreadyParticipate button');
+          const elem = await waitForElm('span.alreadyParticipate');
+          if (!elem) {
+            Logger.debug('onParticipateHandler: no alreadyParticipate button');
+            return;
+          }
+
+          Logger.debug('onParticipateHandler: alreadyParticipate button finded');
+          await sleep(sleepTime);
+          callback();
+        } else {
+          Logger.debug('onParticipateHandler: clicked on disabled contest button');
+        }
+      })
+    })
+    .catch(el => {
+      Logger.debug('onParticipateHandler: no contest button');
+    });
+}
+
+export { onParticipateHandler };
