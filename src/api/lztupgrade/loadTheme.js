a2aeb135cd6eaffb7b928788e693dda9920eaa24
unknown <toil.official@yandex.com>
2023-06-02 00:32:30 +0300

    Rewrited from ajax to fetch

--- src/api/lztupgrade/loadTheme.js
+++ src/api/lztupgrade/loadTheme.js
@@ -2,21 +2,22 @@ import endpoints from 'Configs/endpoints.json' assert {type: 'json'};
 import { Logger } from 'Utils/logger';
 
 async function loadTheme(themeName) {
-  return $.ajax({
-    url: `${endpoints['staticThemes']}/${themeName}.css`,
-    dataType: 'text',
-    success: (value, boolStatus, req) => {
-      if (req.status === 200) {
-        GM_addStyle(value); // ! Maybe need to add polyfills
-        return true;
-      }
-      return false;
-    },
-    error: (err) => {
-      Logger.error(`Не удалось загрузить тему ${themeName} (${endpoints['staticThemes']}/${themeName}.css). Ошибка: ${err}`);
-      return false;
+  try {
+    let res = await fetch(`${endpoints['staticThemes']}/${themeName}.css`)
+
+    if (res.status === 200) {
+      Logger.debug("LoadTheme: Status code 200. Adding theme styles...");
+      const styles = await res.text();
+      GM_addStyle(styles); // ! Maybe need to add polyfills
+      return true;
     }
-  });
+
+    Logger.debug(`LoadTheme: Failed to load theme. Status code: ${res.status}, Status message: ${res.statusText}`);
+    return false;
+  } catch (err) {
+    Logger.error(`Failed to load theme ${themeName} (${endpoints['staticThemes']}/${themeName}.css). Error: `, err);
+    return false;
+  }
 }
 
 export { loadTheme };
