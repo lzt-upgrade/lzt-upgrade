d706a5dea0d226b7415f71f26d206d8b275c8aed
unknown <toil.official@yandex.com>
2023-06-02 00:45:14 +0300

    Added implementation of profileInfoRow class

---
+++ src/ui/kit/profileInfoRow.js
@@ -0,0 +1,42 @@
+import { isElement } from 'Utils/checkers';
+
+class ProfileInfoRow {
+  /**
+   *
+   *  @constructor
+   *  @param {string} elementId - id of the row
+   *  @param {string} label - label of row
+   *  @param {string} content - content of row
+   */
+
+  constructor(elementId, label, content) {
+    this.elementId = elementId;
+    this.label = label;
+    this.content = content;
+  }
+
+  createElement() {
+    const row = document.createElement('div');
+    const label = document.createElement('div');
+    const labeled = document.createElement('div');
+    row.classList.add('clear_fix', 'profile_info_row');
+    row.id = this.elementId;
+
+    label.classList.add('label', 'fl_l');
+    labeled.classList.add('labeled');
+
+    label.innerText = this.label + ':';
+    if (isElement(this.content)) {
+      labeled.appendChild(this.content)
+    } else {
+      labeled.innerHTML = this.content;
+    }
+
+    row.appendChild(label)
+    row.appendChild(labeled)
+
+    return row;
+  }
+}
+
+export { ProfileInfoRow };
