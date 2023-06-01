b10da3d628fe7bae07f070abc4b3a09cc057d6ba
unknown <toil.official@yandex.com>
2023-06-02 00:36:12 +0300

    Added event on click to selected category

---
+++ src/events/categories.js
@@ -0,0 +1,12 @@
+import { waitForElm, sleep } from 'Utils/utils';
+
+function onClickCategory(nodeSelector, callback) {
+  const node = $(`li.node${nodeSelector}.forum.level-n`);
+  $(node).on('click', async () => {
+    await sleep(750);
+    const el = await waitForElm('div.pageNavLinkGroup');
+    if (el) callback();
+  });
+}
+
+export default onClickCategory;
