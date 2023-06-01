2035653fd47efd46449e2280af6cfd42f6581db0
unknown <toil.official@yandex.com>
2023-06-02 00:54:28 +0300

    Added comment for future

--- src/utils/threads.js
+++ src/utils/threads.js
@@ -1,3 +1,4 @@
+// СОЗДАТЬ ОТДЕЛЬНУЮ ФУНКЦИЮ ДЛЯ ЭТОГО А ТО АХУЕТЬ СКОК ДУБЛИРУЕТСЯ
 function hideThreadContent(isHidden) {
   const messageContent = document.querySelector('.message.firstPost > .messageInfo > .messageContent > article > blockquote.messageText');
   if (messageContent !== null) {
