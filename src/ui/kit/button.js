cec8867c2f22848bf61b55d29ee743738b717b33
unknown <toil.official@yandex.com>
2023-06-02 00:42:38 +0300

    added implementation of Button class

---
+++ src/ui/kit/button.js
@@ -0,0 +1,33 @@
+import { createMenuIcon } from "UI/kit/icons.js";
+
+class Button {
+  /**
+   *
+   *  @constructor
+   *  @param {string} buttonText - text of the button
+   *  @param {string} className - class name of the button
+   *  @param {string} iconClassName - class name of the icon (if undefined icon is not added)
+   */
+
+  constructor(buttonText, className = 'button', iconClassName = undefined) {
+    this.buttonText = buttonText;
+    this.className = className;
+    this.iconClassName = iconClassName;
+  }
+
+  createElement() {
+    const button = document.createElement('button');
+    button.className = this.className;
+    button.innerText = this.buttonText;
+
+    if (this.iconClassName) {
+      const icon = createMenuIcon(this.iconClassName);
+      icon.id = '';
+      button.appendChild(icon);
+    }
+
+    return button;
+  }
+}
+
+export { Button };
