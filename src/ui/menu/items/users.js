a29a27f77c103b284a7a936167b14c9b6b5301f8
unknown <toil.official@yandex.com>
2023-06-02 16:34:40 +0300

    Show ID is divided into show in profile and show in user profile

--- src/ui/menu/items/users.js
+++ src/ui/menu/items/users.js
@@ -1,6 +1,6 @@
 import { LZTUsersDB } from "IndexedDB/users";
 import { Checkbox } from 'UI/menu/checkbox';
-import { addUserId, removeUserId, showFullRegDateInProfile } from 'Utils/users';
+import { addUserIdToProfile, removeUserIdFromProfile, showFullRegDateInProfile } from 'Utils/users';
 import { registerAlert } from "Utils/registers";
 import { sleep } from "Utils/utils";
 
@@ -9,16 +9,34 @@ const getUsersItems = async () => {
   const usersData = await usersDB.read();
 
   return [
-    new Checkbox('show_userid', 'Показывать UserID пользователей')
+    new Checkbox('show_userid_in_profile', 'Показывать ID в профиле пользователя')
     .createElement(
-      usersData.showUserId,
+      usersData.showUserIdInProfile,
       async () => {
-        await usersDB.update({ showUserId: 1 });
-        addUserId();
+        await usersDB.update({ showUserIdInProfile: 1 });
+        addUserIdToProfile();
       },
       async () => {
-        await usersDB.update({ showUserId: 0 });
-        removeUserId();
+        await usersDB.update({ showUserIdInProfile: 0 });
+        removeUserIdFromProfile();
+      }),
+    new Checkbox('show_userid_in_member_card',
+      `Показывать ID в карточке пользователя
+      <span class="fa fa-exclamation-triangle Tooltip" title="При включение/отключение этой функции страница будет перезагружена"></span>
+      `)
+    .createElement(
+      usersData.showUserIdInMemberCard,
+      async () => {
+        await usersDB.update({ showUserIdInMemberCard: 1 });
+        registerAlert('Показывать ID в карточке пользователя включено', 5000);
+        await sleep(500);
+        window.location.reload();
+      },
+      async () => {
+        await usersDB.update({ showUserIdInMemberCard: 0 });
+        registerAlert('Показывать ID в карточке пользователя выключено', 5000);
+        await sleep(500);
+        window.location.reload();
       }),
     new Checkbox('show_fullreg_in_profile', 'Показывать полную дату регистрации в профиле пользователя')
     .createElement(
@@ -39,13 +57,13 @@ const getUsersItems = async () => {
       usersData.disableShareTyping,
       async () => {
         await usersDB.update({ disableShareTyping: 1 });
-        registerAlert('Отправка информации о наборе сообщения отключена', 5000);
+        registerAlert('Неписалка в темах включена', 5000);
         await sleep(500);
         window.location.reload();
       },
       async () => {
         await usersDB.update({ disableShareTyping: 0 });
-        registerAlert('Отправка информации о наборе сообщения включена', 5000);
+        registerAlert('Неписалка в темах выключена', 5000);
         await sleep(500);
         window.location.reload();
       }),
