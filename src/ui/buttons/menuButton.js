286f700f39cb6fb816c067532a929893c7f15d09
unknown <toil.official@yandex.com>
2023-06-07 02:06:48 +0300

    html string rewrited to element

--- src/ui/buttons/menuButton.js
+++ src/ui/buttons/menuButton.js
@@ -4,8 +4,11 @@ import config from 'Configs/config';
 import 'Styles/buttons.scss';
 
 const menuButton = document.createElement('li');
-menuButton.innerHTML = `<a id="LZTUpButton">${config.extName}</a>`;
+const link = document.createElement('a');
+link.id = 'LZTUpButton';
+link.innerText = config.extName;
 
+menuButton.appendChild(link);
 menuButton.onclick = menuButtonCallback;
 
 export default menuButton;
