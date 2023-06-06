f23d9792838360b7b772dd54ce910f740d94c2e5
unknown <toil.official@yandex.com>
2023-06-07 02:07:39 +0300

    Added description menu element

---
+++ src/ui/kit/menu/description.js
@@ -0,0 +1,21 @@
+class Description {
+  /**
+   *
+   *  @constructor
+   *  @param {string} text - text of the description
+   */
+
+  constructor(text) {
+    this.text = text;
+  }
+
+  createElement() {
+    const desc = document.createElement('p');
+    desc.classList.add('LZTUpModalDescription', 'muted');
+    desc.innerText = this.text;
+
+    return desc;
+  }
+}
+
+export { Description };
