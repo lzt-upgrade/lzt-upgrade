d530e3f6c81d5f9d34b1d32923bff2f8b2eebc6d
unknown <toil.official@yandex.com>
2023-06-07 02:07:18 +0300

    Added colorpicker menu element

---
+++ src/ui/kit/menu/colorPicker.js
@@ -0,0 +1,39 @@
+import { Description } from 'UI/kit/menu/description';
+
+class ColorPicker {
+  /**
+   *
+   *  @constructor
+   *  @param {string} id - id of the color picker
+   *  @param {string} value - value of the color picker
+   */
+
+  constructor(id, value, description = '') {
+    this.id = id
+    this.value = value;
+    this.description = description;
+  }
+
+  createElement(callback) {
+    const wrap = document.createElement('div');
+    wrap.classList.add('LZTUpColorPickerWrap');
+
+    const input = document.createElement('input');
+    input.id = this.id;
+    input.classList.add('LZTUpColorPicker');
+    input.value = this.value;
+    input.type = 'text';
+    input.dataset.coloris = '';
+    input.oninput = callback;
+
+    if (this.description !== '') {
+      const description = new Description(this.description).createElement();
+      wrap.appendChild(description);
+    }
+
+    wrap.appendChild(input);
+    return wrap;
+  }
+}
+
+export { ColorPicker };
