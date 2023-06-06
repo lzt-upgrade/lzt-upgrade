9527793cdd2c994bb6a6341e4808ef14f989c819
unknown <toil.official@yandex.com>
2023-06-07 01:44:47 +0300

    Moved all functions to Xenforo/tooltips.js, added setTooltip function

---
+++ src/xenforo/tooltips.js
@@ -0,0 +1,13 @@
+function updateTooltips() {
+  let lztUpTooltips = $('#LZTUpTooltip.Tooltip');
+  return XenForo.Tooltip(lztUpTooltips);
+}
+
+function setTooltip(el, text) {
+  el.setAttribute('title', '');
+  el.setAttribute('data-cachedtitle', text);
+
+  return el._tippy.setContent(text);
+}
+
+export { updateTooltips, setTooltip }
