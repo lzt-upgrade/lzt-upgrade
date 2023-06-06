e5bc5c2a356219684c7ad6c0d1b9d55a1b5a8ae5
unknown <toil.official@yandex.com>
2023-06-07 01:46:24 +0300

    Added functions for change svg colors

---
+++ src/utils/svg.js
@@ -0,0 +1,21 @@
+function setSvgAttr(el, type, color) {
+  return el.setAttribute(type, color);
+}
+
+function changeSVGColor(el, type, color, replaceAll = false) {
+  if (!['stroke', 'fill'].includes(type)) {
+    return;
+  }
+
+  if (!replaceAll) {
+    return setSvgAttr(el, type, color);
+  }
+
+  setSvgAttr(el, type, color);
+  const elements = el.querySelectorAll('*');
+  for (const element of elements) {
+    setSvgAttr(element, type, color);
+  }
+}
+
+export { changeSVGColor };
