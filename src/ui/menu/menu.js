bf8096b8880038dcc0c16da039ae806ada0afea2
unknown <toil.official@yandex.com>
2023-06-07 02:11:08 +0300

    Added profile section, removed .tabs class from tabs container element

--- src/ui/menu/menu.js
+++ src/ui/menu/menu.js
@@ -1,10 +1,10 @@
 import { MenuSection } from 'UI/kit/menuSection';
 import { addMenuSectionItem } from 'UI/menu/sectionItem';
-import { Comment } from 'UI/menu/comment';
 import { addMenuSectionContainer } from 'UI/menu/sectionContainer';
 import { Logger } from 'Utils/logger';
 import getContestsItems from 'UI/menu/items/contests';
 import getUsersItems from 'UI/menu/items/users';
+import getProfileItems from 'UI/menu/items/profile';
 import 'Styles/menu.scss';
 
 
@@ -16,29 +16,15 @@ async function generateMenu(tabs) {
     addMenuSectionItem('Внешний вид', 'Темы, логотипы и другое', 'far fa-drafting-compass', 'LZTUpAppearItem', 'LZTUpUniqContainer'),
   ];
 
-  Logger.debug('Generated menu main items: ', mainItems)
-
   const settingsItems = [
     addMenuSectionItem('Настройки', 'Настройки расширения', 'far fa-cog', 'LZTUpSettingsItem', 'LZTUpUniqContainer'),
     addMenuSectionItem('Обновления', 'Установка и проверка обновлений расширения', 'far fa-cloud-download', 'LZTUpUpdateItem', 'LZTUpUpdateContainer'),
     addMenuSectionItem('Информация', `Версия: ${GM_info?.script?.version}`, 'far fa-info-circle', 'LZTUpInformationItem', 'LZTUpUniqContainer'),
   ];
 
-  Logger.debug('Generated menu settings items: ', settingsItems)
   const sections = [
     new MenuSection('LZTUpMainSection', mainItems).create(),
     new MenuSection('LZTUpSettingsSection', settingsItems).create(),
-  ];
-
-  Logger.debug('Generated menu sections: ', sections)
-
-  const uniqueText = document.createElement('div')
-  uniqueText.innerText = 'Страница локального уника';
-
-  const uniqueItems = [
-    new Comment(`Это оформление видно только вам. Мы рекомендуем вам <a href="https://${window.location.hostname}/account/upgrades?upgrade_id=14" target="_blank">приобрести настоящий Уник</a>, чтобы все пользователи смогли увидеть ваше настоящее оформление профиля.`)
-      .createElement(),
-    uniqueText,
   ];
 
   const updateText = document.createElement('div')
@@ -48,10 +34,8 @@ async function generateMenu(tabs) {
     updateText,
   ];
 
-  Logger.debug('Generated menu unique section items: ', uniqueItems)
-
   const sectionContainers = [
-    addMenuSectionContainer('LZTUpUniqContainer', uniqueItems),
+    addMenuSectionContainer('LZTUpUniqContainer', await getProfileItems()),
     addMenuSectionContainer('LZTUpContestsContainer', await getContestsItems()),
     addMenuSectionContainer('LZTUpUsersContainer', await getUsersItems()),
     addMenuSectionContainer('LZTUpUpdateContainer', updateItems),
@@ -63,7 +47,7 @@ async function generateMenu(tabs) {
   menuContent.classList.add('LZTUpModalContent');
 
   const tabsContainer = document.createElement('ul');
-  tabsContainer.classList.add('tabs', 'LZTUpTabs');
+  tabsContainer.classList.add('LZTUpTabs');
   menuContent.appendChild(tabsContainer);
 
   for (const section of sections) {
