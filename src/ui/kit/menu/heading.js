d43b628d2ce501d84b82c5e5b3c9d3e2da3d38fb
unknown <toil.official@yandex.com>
2023-06-07 02:07:48 +0300

    Added heading menu element

---
+++ src/ui/kit/menu/heading.js
@@ -0,0 +1,21 @@
+class Heading {
+  /**
+   *
+   *  @constructor
+   *  @param {string} text - text of the heading
+   */
+
+  constructor(text) {
+    this.text = text;
+  }
+
+  createElement() {
+    const heading = document.createElement('h2');
+    heading.classList.add('LZTUpModalHeading');
+    heading.innerText = this.text;
+
+    return heading;
+  }
+}
+
+export { Heading };
