b71f41eb848250f696003df839e3cbb28090a871
unknown <toil.official@yandex.com>
2023-07-12 13:16:55 +0300

    added semi-full profile section content

--- src/ui/menu/items/profile.js
+++ src/ui/menu/items/profile.js
@@ -4,24 +4,230 @@ import { Comment } from 'UI/menu/comment';
 import { PreviewProfile } from 'UI/kit/menu/previewProfile';
 import { TextArea } from 'UI/kit/menu/textArea';
 import { Input } from 'UI/kit/menu/input';
+import { Button } from 'UI/kit/button';
 import { ColorPicker } from 'UI/kit/menu/colorPicker';
 import { Container } from 'UI/kit/menu/container';
 import { Separator } from 'UI/kit/menu/separator';
+import { SortableContainer } from 'UI/kit/menu/sortableContainer';
+import { SortableItem } from 'UI/kit/menu/sortableItem';
 import { getUserId, getUsername } from 'Utils/users';
 import { registerAlert } from 'Utils/registers';
-import { clearSVG } from 'Utils/utils';
+import { clearSVG, clearHTML } from 'Utils/purify';
+import { addTemporaryMenuSection, openTempMenu } from 'UI/menu/temporarySection';
+import { initColorPickers } from 'Utils/colorPicker';
+import { AvatarUserBadges } from 'UI/avatarUserBadges';
+import { updateUserStyle, updateUserBanner } from 'Visuals/users';
 
-const getProfileItems = async () => {
-  const profileDB = new LZTProfileDB();
+const profileDB = new LZTProfileDB();
+
+async function sortableItemOnEditCallback(e, previewProfile) {
+  const badgeId = Number(e.target.parentElement.parentElement.parentElement.dataset?.id);
+  const modalContent = document.querySelector('.LZTUpModalContent');
+  const uniqSubMenu = document.querySelector('.LZTUpUniqContainer');
   const profileData = await profileDB.read();
-  const currentDomain = window.location.hostname;
+  const thisBadgeArray = profileData.badgeIcons.filter(icon => icon.position === badgeId);
+  let badgeData = thisBadgeArray[0];
+
+  async function updateBadgesData(badgeData) {
+    // badgeData its current badge (which we are editing)
+    const profData = await profileDB.read();
+    const badges = profData.badgeIcons.filter(icon => icon.position !== badgeId);
+    badges.push(badgeData);
+    return badges;
+  }
+
+  const el = addTemporaryMenuSection(
+    [
+      new Container(
+        [
+          new TextArea(badgeData.svg, '<svg>...</svg>', 0, 3000)
+          .createElement(
+            async (event) => {
+              let val = clearSVG(event.target.value.trim());
+
+              if (val.length > 3000) {
+                return registerAlert('Максимальная длина иконки 3000 символов. Уменьшите введенный текст для сохранения.')
+              }
+
+              badgeData.svg = val;
+            }
+          ),
+        ],
+        'Иконка на аватарке',
+        'Максимум 3000 символов.',
+      ).createElement(),
+
+      new Container(
+        [
+          new TextArea(badgeData.style, 'background: #fff', 0, 1500)
+          .createElement(
+            async (event) => {
+              let val = event.target.value;
+
+              if (val.length > 1500) {
+                return registerAlert('Максимальная длина стиля иконки 1500 символов. Уменьшите введенный текст для сохранения.')
+              }
+
+              if (val.length > 1 && val.startsWith('.')) {
+                event.target.value = val.replace(/\s/g, '');
+                val = event.target.value;
+              }
+
+              badgeData.style = val;
+            }
+          ),
+        ],
+        'Стиль иконки',
+        'Максимум 1500 символов.',
+      ).createElement(),
+
+      new Container(
+        [
+          new Input(badgeData.text, 'Идут два сталкера', 0, 24)
+          .createElement(
+            async (event) => {
+              let val = event.target.value;
+
+              if (val.length > 24) {
+                return registerAlert('Максимальная длина текста в лычке 24 символа. Уменьшите введенный текст для сохранения.')
+              }
+
+              badgeData.text = val;
+            }
+          ),
+        ],
+        'Текст при наведение на иконку',
+        'Максимум 24 символа.',
+      ).createElement(),
+
+      new Container(
+        [
+          new ColorPicker('LZTUpColorPickerFill', badgeData.fillColor, 'Цвет иконки (fill):')
+          .createElement(
+            async (event) => {
+              let val = event.target.value;
+
+              badgeData.fillColor = val;
+            }
+          ),
+          new ColorPicker('LZTUpColorPickerStroke', badgeData.strokeColor, 'Цвет иконки (stroke):')
+          .createElement(
+            (event) => {
+              let val = event.target.value;
+
+              badgeData.strokeColor = val;
+            }
+          ),
+        ],
+        'Изменение цвета иконки'
+      ).createElement(),
+
+      new Container([
+        new Button('Сохранить', 'button primary LZTUpIconButton fit', 'far fa-save').createElement(async () => {
+          registerAlert('Иконка успешно сохранена.')
+          const badges = await updateBadgesData(badgeData);
+          await profileDB.update({ badgeIcons: badges });
+        }),
+      ]).createElement()
+    ]
+  )
+
+  modalContent.appendChild(el);
+  initColorPickers();
+  openTempMenu('Управление иконкой', 'Локальный Уник', uniqSubMenu, async () => {
+    const sortable = document.querySelectorAll('.LZTUpSortableContainer > .LZTUpSortableItem');
+    const profileData = await profileDB.read();
+    for (let i = 0; i < profileData.badgeIcons.length; i++) {
+      const content = sortable[i].querySelector('.LZTUPSortableContent');
+      content.innerHTML = clearHTML(profileData.badgeIcons[i].text);
+    }
+
+    previewProfile.data = profileData;
+    previewProfile.updateAll();
+  });
+}
+
+function createPreviewProfile(profileData) {
   const userid = getUserId('me');
   const username = getUsername('me');
+  return new PreviewProfile(userid, username, profileData);
+}
 
-  const previewProfile = new PreviewProfile(userid, username, profileData);
+const getProfileItems = async () => {
+  function generateBadgeItems(previewProfile, profileData) {
+    const items = [];
+    for (const badge of profileData.badgeIcons) {
+      items.push(new SortableItem(badge.text, badge.position).createElement((e) => sortableItemOnEditCallback(e, previewProfile), sortableItemOnRemoveCallback));
+    }
+
+    return items;
+  }
+
+  function reloadUserBadges(updatedProfileData) {
+    const avatarUserBadgesParent = document.querySelector('#LZTUpPreviewContainer > .avatarBox > .avatarUserBadges');
+    if (avatarUserBadgesParent) {
+      for (const userBadge of avatarUserBadgesParent.children) {
+        userBadge.remove();
+      }
+
+      const avatarUserBadges = new AvatarUserBadges(updatedProfileData.badgeIcons, true).createElement();
+      avatarUserBadgesParent.innerHTML = avatarUserBadges.innerHTML;
+
+      previewProfile.data = updatedProfileData;
+      previewProfile.updateAll();
+    }
+  }
+
+  async function sortableItemOnRemoveCallback(e, sortableItemEl) {
+    const profileData = await profileDB.read();
+    let badgeIcons = profileData.badgeIcons;
+    let newBadgeIcons = [];
+
+    const badgeEl = document.querySelector(`#LZTUpPreviewBadge[data-position="${sortableItemEl.dataset.id}"]`)
+    if (badgeEl) {
+      badgeEl.remove();
+    }
+
+    const remainBadgeEl = document.querySelector('#LZTUpPreviewBadge')
+    if (remainBadgeEl) {
+      remainBadgeEl.dataset.position = 1;
+      remainBadgeEl.dataset.multiple = "false";
+      if (remainBadgeEl?.classList.contains('avatarUserBadge--1')) {
+        remainBadgeEl.classList.remove('avatarUserBadge--1');
+      }
+
+      if (remainBadgeEl?.classList.contains('avatarUserBadge--2')) {
+        remainBadgeEl.classList.remove('avatarUserBadge--2');
+      }
+    }
+
+    for (const badge of badgeIcons) {
+      if (String(badge.position) === sortableItemEl.dataset.id) {
+        continue;
+      }
+
+      // if removed 1st badge set 2nd badge to 1st position
+      const item = document.querySelector('.LZTUpSortableContainer > .LZTUpSortableItem');
+      if (item) {
+        item.dataset.id = 1;
+      }
+
+      badge.position = 1;
+      newBadgeIcons.push(badge)
+    }
+
+    await profileDB.update({ badgeIcons: newBadgeIcons });
+    previewProfile.data = profileData;
+    previewProfile.updateAll();
+  }
+
+  const profileData = await profileDB.read();
+  const currentDomain = window.location.hostname;
+
+  const previewProfile = createPreviewProfile(profileData);
 
   return [
-    new Comment(`В этой вкладке можно выбрать стиль вашего ника и лычки. Этот стиль виден только вам.
+    new Comment(`На этой вкладке вы можете выбрать стиль вашего ника и лычки. Этот стиль виден только вам.
       Чтобы уник был виден всем, рекомендуем <a href="https://${currentDomain}/account/upgrades?upgrade_id=14" target="_blank">купить</a> настоящий уник.`)
       .createElement(),
 
@@ -43,7 +249,7 @@ const getProfileItems = async () => {
             }
 
             previewProfile.updateUsernameStyle(val);
-            profileDB.update({ usernameStyle: val }); // ! REMOVE AFTER TESTS
+            profileData.usernameStyle = val;
           }
         ),
       ],
@@ -66,10 +272,8 @@ const getProfileItems = async () => {
               val = event.target.value;
             }
 
-            previewProfile.updateBannerStyle(val);
-            previewProfile.updateBadgeStyle(val);
-
-            profileDB.update({ bannerStyle: val }); // ! REMOVE AFTER TESTS
+            profileData.bannerStyle = val;
+            previewProfile.updateBanner(profileData);
           }
         ),
       ],
@@ -87,8 +291,8 @@ const getProfileItems = async () => {
               return registerAlert('Максимальная длина текста в лычке 24 символа. Уменьшите введенный текст для сохранения.')
             }
 
-            previewProfile.updateBannerText(val);
-            profileDB.update({ bannerText: val }); // ! REMOVE AFTER TESTS
+            profileData.bannerText = val;
+            previewProfile.updateBanner(profileData);
           }
         ),
       ],
@@ -99,85 +303,80 @@ const getProfileItems = async () => {
 
     new Separator().createElement(), // * ADD SEPARATOR
 
-
     new Container(
       [
-        new TextArea(profileData.badgeIcon, '<svg>...</svg>', 0, 3000)
-        .createElement(
-          async (event) => {
-            // event.target.value = clearSVG(event.target.value); // ! add to save button
-            let val = clearSVG(event.target.value.trim());
-
-            if (val.length > 3000) {
-              return registerAlert('Максимальная длина иконки 3000 символов. Уменьшите введенный текст для сохранения.')
-            }
-
-            let data = await profileDB.read();
-            data['badgeIcon'] = val
-            previewProfile.updateBadgeAll(data);
-
-            profileDB.update({ badgeIcon: val }); // ! REMOVE AFTER TESTS
+        new SortableContainer(
+          generateBadgeItems(previewProfile, profileData)
+        ).createElement(async (e) => {
+          // move items
+          const items = e.target.children;
+          const newProfileData = await profileDB.read();
+          newProfileData.badgeIcons.reverse();
+          for (let i = 0; i < items.length; i++) {
+            items[i].dataset.id = i + 1;
+            newProfileData.badgeIcons[i].position = i + 1;
           }
-        ),
+
+          profileData.badgeIcons = newProfileData.badgeIcons;
+          await profileDB.update({ badgeIcons: newProfileData.badgeIcons });
+          reloadUserBadges(profileData);
+        }),
+
+        new Button('Добавить иконку', 'button LZTUpIconButton', 'far fa-plus')
+        .createElement(async (e) => {
+          const sortableContainer = e.target.parentElement?.querySelector('.LZTUpSortableContainer');
+
+          if (!sortableContainer) {
+            return registerAlert('Не найден контейнер для добавления!')
+          }
+
+          if (sortableContainer.children.length === 2) {
+            return registerAlert('Вы не можете добавить больше 2 иконок!')
+          }
+
+          const newProfileData = await profileDB.read();
+          const badgeIcons = newProfileData.badgeIcons;
+
+          const defaultIcon = {
+            'position': sortableContainer.children.length + 1,
+            'style': '',
+            'text': 'Новая иконка',
+            'svg': '',
+            'fillColor': '',
+            'StrokeColor': '',
+          }
+
+          badgeIcons.push(defaultIcon)
+
+          const newItem = new SortableItem(defaultIcon.text, defaultIcon.position).createElement((e) => sortableItemOnEditCallback(e, previewProfile), sortableItemOnRemoveCallback);
+
+          sortableContainer.appendChild(newItem);
+          await profileDB.update({ badgeIcons: badgeIcons });
+
+          profileData.badgeIcons = badgeIcons;
+
+          reloadUserBadges(profileData);
+        }),
       ],
-      'Иконка на аватарке',
-      'Максимум 3000 символов.',
+      'Управление иконками',
+      'Ниже вы можете легко настроить иконки уника и их порядок (изменения автоматически применяются)'
     ).createElement(),
 
-    new Container(
-      [
-        new Input(profileData.badgeText, 'Идут два сталкера', 0, 24)
-        .createElement(
-          (event) => {
-            let val = event.target.value;
-            if (val.length > 24) {
-              return registerAlert('Максимальная длина текста в лычке 24 символа. Уменьшите введенный текст для сохранения.')
-            }
+    new Container([
+      new Button('Сохранить', 'button primary LZTUpIconButton fit', 'far fa-save').createElement(async () => {
+        registerAlert('Настройки локального уника успешно сохранены.');
+        await profileDB.update({ usernameStyle: profileData.usernameStyle });
+        await profileDB.update({ bannerStyle: profileData.bannerStyle });
+        await profileDB.update({ bannerText: profileData.bannerText });
+        if (profileData.usernameStyle) {
+          updateUserStyle(profileData.usernameStyle);
+        }
 
-            previewProfile.updateBadgeText(val);
-            profileDB.update({ badgeText: val }); // ! REMOVE AFTER TESTS
-          }
-        ),
-      ],
-      'Текст при наведение на иконку',
-      'Максимум 24 символа.',
-    ).createElement(),
-
-    new Container(
-      [
-        new ColorPicker('LZTUpColorPickerFill', profileData.badgeFill, 'Цвет иконки (fill):')
-        .createElement(
-          (event) => {
-            let val = event.target.value;
-
-            previewProfile.updateBadgeColor(val, 'fill');
-            profileDB.update({ badgeFill: val }); // ! REMOVE AFTER TESTS
-          }
-        ),
-        new ColorPicker('LZTUpColorPickerStroke', profileData.badgeStroke, 'Цвет иконки (stroke):')
-        .createElement(
-          (event) => {
-            let val = event.target.value;
-
-            previewProfile.updateBadgeColor(val, 'stroke');
-            profileDB.update({ badgeStroke: val }); // ! REMOVE AFTER TESTS
-          }
-        ),
-      ],
-      'Изменение цвета иконки'
-    ).createElement(),
-
-    // new Checkbox('open_ten_contests', 'Кнопка "Открыть 10"')
-    // .createElement(
-    //   profileData.openTenContestsBtn,
-    //   async () => {
-    //     await profileData.update({openTenContestsBtn: 1});
-    //     regOpenContestsBtn(10);
-    //   },
-    //   async () => {
-    //     await profileData.update({openTenContestsBtn: 0});
-    //     removeOpenContestsBtn(10);
-    //   }),
+        if (profileData.bannerStyle && profileData.bannerText) {
+          updateUserBanner(profileData.bannerStyle, profileData.bannerText);
+        }
+      }),
+    ]).createElement()
   ];
 }
 
