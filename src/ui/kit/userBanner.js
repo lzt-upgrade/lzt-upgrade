04de1b7461a74df990c94c97ae2000c4e50a5642
unknown <toil.official@yandex.com>
2023-07-12 13:14:01 +0300

    Added user banner to user profile

---
+++ src/ui/kit/userBanner.js
@@ -0,0 +1,34 @@
+class UserBanner {
+  /**
+   *
+   *  @constructor
+   *  @param {string} elementId - id of the row
+   *  @param {string} label - label of user banner
+   */
+
+  constructor(elementId, label) {
+    this.elementId = elementId;
+    this.label = label;
+  }
+
+  createElement() {
+    const userBanner = document.createElement('em');
+    userBanner.id = this.elementId;
+    userBanner.classList.add('userBanner', 'wrapped');
+    userBanner.itemprop = 'title';
+
+    const beforeAfterEl = document.createElement('div');
+    beforeAfterEl.className = 'before';
+    userBanner.appendChild(beforeAfterEl);
+
+    const bannerContent = document.createElement('strong');
+    bannerContent.innerText = XenForo.htmlspecialchars(this.label);
+    userBanner.appendChild(bannerContent)
+
+    beforeAfterEl.className = 'after';
+    userBanner.appendChild(beforeAfterEl)
+    return userBanner;
+  }
+}
+
+export { UserBanner };
