6bba6f413abadaee0b2089467fc27608d09da538
unknown <toil.official@yandex.com>
2023-07-20 02:51:45 +0300

    remaked self ads buttons on error page

---
+++ src/ui/buttons/errorPageButton.js
@@ -0,0 +1,26 @@
+class ErrorPageButton {
+  /**
+   *
+   *  @constructor
+   *  @param {string} buttonContent - content of the button
+   *  @param {string} buttonHref - link of the button (https://...)
+   *  @param {string} className - class name of the button
+   */
+
+  constructor(buttonContent, buttonHref = '#', className = 'button') {
+    this.buttonContent = buttonContent;
+    this.buttonHref = buttonHref;
+    this.className = className;
+  }
+
+  createElement() {
+    const button = document.createElement('a');
+    button.href = this.buttonHref;
+    button.className = this.className;
+    button.innerHTML = this.buttonContent;
+
+    return button;
+  }
+}
+
+export default ErrorPageButton;
