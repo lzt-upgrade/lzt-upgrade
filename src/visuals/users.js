77567b9edf85e8094026845b6b50dcc253e74790
unknown <toil.official@yandex.com>
2023-07-20 02:58:03 +0300

    added update user badges function

--- src/visuals/users.js
+++ src/visuals/users.js
@@ -2,6 +2,7 @@ import { applyStyle } from "Utils/utils";
 import { getUsername, getUserId } from 'Utils/users';
 import { isProfilePage } from 'Utils/checkers';
 import { UserBanner } from 'UI/kit/userBanner';
+import { AvatarUserBadges } from 'UI/avatarUserBadges';
 
 function updateUserStyle(style) {
   const username = getUsername('me');
@@ -40,7 +41,31 @@ function updateUserBanner(style, text) {
   }
 }
 
+function updateUserBadges(badgeIconsData) {
+  const userid = getUserId('me');
+  const badges = new AvatarUserBadges(badgeIconsData, false);
+  const avatarsMedium = document.querySelectorAll(`.avatar.Av${userid}m`);
+  const avatarsSmall = document.querySelectorAll(`.avatar.Av${userid}s`);
+  const avatars = [...avatarsMedium, ...avatarsSmall]
+                  .filter(el => el !== undefined)
+                  .filter(el => el.parentElement?.classList.contains('avatarHolder'));
+  console.log(avatars) // TODO: remove after tests
+  for (const avatar of avatars) {
+    console.log(avatar) // TODO: remove after tests
+    const avatarHolder = avatar.parentElement;
+    const badgesEl = badges.createElement();
+
+    console.log(avatarHolder) // TODO: remove after tests
+    avatarHolder.querySelector('.avatarUserBadges')?.remove(); // remove if exists
+    console.log(badgesEl) // TODO: remove after tests
+    // avatarHolder.insertAdjacentElement('afterbegin', badgesEl);
+    avatarHolder.prepend(badgesEl)
+    badges.updateBadges()
+  }
+}
+
 export {
   updateUserStyle,
-  updateUserBanner
+  updateUserBanner,
+  updateUserBadges
 }
