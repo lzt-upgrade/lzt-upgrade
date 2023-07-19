6fbd4ddbca52c2d2ccbf7fd81295772eaf3e4a89
unknown <toil.official@yandex.com>
2023-07-20 02:56:27 +0300

    added purify css

--- src/utils/purify.js
+++ src/utils/purify.js
@@ -1,3 +1,16 @@
+import { Logger } from "Utils/logger";
+
+const defaultAvailabledStyles = [
+  'color',
+  'text-shadow',
+  'border-radius',
+  'background',
+  'background-color',
+  'background-image',
+  '-webkit-background-clip',
+  '-webkit-text-fill-color'
+]
+
 function clearHTML(element) {
   return DOMPurify.sanitize(element, {
     USE_PROFILES: {
@@ -21,7 +34,28 @@ function clearSVG(element) {
   });
 }
 
+// removes all prohibited styles from the string
+function clearCSS(css, availabledStyles = defaultAvailabledStyles) {
+  let pairs = css.split(';');
+  const goodCSS = [];
+  Logger.debug(pairs);
+  for (const pair of pairs) {
+    const values = pair.split(':');
+    const key = values[0].replace(/\s/, '');
+    Logger.debug(pair, values, key)
+    if (availabledStyles.includes(key)) {;
+      Logger.debug('DETECTED AVAILABLED STYLES');
+      goodCSS.push(pair);
+    }
+  }
+
+  const cssString = goodCSS.length ? goodCSS.join(';') : '';
+  Logger.debug(`CLEARED FINAL STRING: ${cssString}`);
+  return cssString;
+}
+
 export {
   clearHTML,
-  clearSVG
+  clearSVG,
+  clearCSS
 }
