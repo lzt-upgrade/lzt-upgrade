a51602d835cc51f15e2d17812fb7187a55336dc6
unknown <toil.official@yandex.com>
2023-09-12 23:01:25 +0300

    added temporary plug

---
+++ src/ui/menu/items/info.js
@@ -0,0 +1,10 @@
+import { Description } from 'UI/kit/menu/description.js';
+
+const getInfoItems = async () => {
+
+  return [
+    new Description('Информационное меню').createElement()
+  ];
+}
+
+export default getInfoItems;
