a32fed85303f1ed8ca494fcde82f273a6fc15e72
unknown <toil.official@yandex.com>
2023-09-14 01:27:38 +0300

    added minify support

--- webpack.config.js
+++ webpack.config.js
@@ -18,115 +18,130 @@ const ru_headers = getHeaders('locales/ru/headers.json');
 
 console.log('development mode: ', dev);
 
-export default {
-  mode: dev ? 'development' : 'production',
-  resolve: {
-    extensions: [".js"],
-    alias: {
-      API: path.resolve(__dirname, 'src/api/'),
-      Events: path.resolve(__dirname, 'src/events/'),
-      IndexedDB: path.resolve(__dirname, 'src/indexedDB/'),
-      Callbacks: path.resolve(__dirname, 'src/callbacks/'),
-      Configs: path.resolve(__dirname, 'src/configs/'),
-      Styles: path.resolve(__dirname, 'src/styles/'),
-      UI: path.resolve(__dirname, 'src/ui/'),
-      Utils: path.resolve(__dirname, 'src/utils/'),
-      Xenforo: path.resolve(__dirname, 'src/xenforo/'),
-      Visuals: path.resolve(__dirname, 'src/visuals/'),
-    },
-  },
-  performance: {
-    hints: "error",
-    maxEntrypointSize: 2000 * 10 ** 3,
-    maxAssetSize: 2000 * 10 ** 3,
-  },
-  entry: path.resolve(__dirname, 'src', 'index.js'),
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'lzt-upgrade.user.js',
-    publicPath: "/",
-  },
-  devServer: {
-    server: "http",
-    port: 11944,
-    allowedHosts: "all",
-    hot: true,
-    liveReload: false,
-    magicHtml: false,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Headers": "*",
-    },
-    // client: {
-    //   webSocketURL: "ws://localhost:11944/ws",
-    //   progress: true,
-    //   reconnect: false
-    // },
-    client: false
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      DEV_MODE: JSON.stringify(dev)
-    }),
-    new UserscriptPlugin({
-      headers: async () => {
-        const headerPath = path.resolve(__dirname, 'src', 'headers.json');
-        const headers = JSON.parse(fs.readFileSync(headerPath).toString());
+export default (env) => {
+  const build_type = env.build_type;
+  console.log('build type: ', build_type);
 
-        let version = headers.version;
-        let name = headers.name;
-        if (dev) {
-          headers['name'] = `[DEV] ${name}`
-          headers["version"] = `${version}-build.[buildNo]`;
-        }
+  function get_filename() {
+    let name = 'lzt-upgrade'
 
-        return headers;
+    if (build_type === 'minify') {
+      name += '-min'
+    }
+
+    return name + '.js'
+  }
+
+  return {
+    mode: dev ? 'development' : 'production',
+    resolve: {
+      extensions: [".js"],
+      alias: {
+        API: path.resolve(__dirname, 'src/api/'),
+        Events: path.resolve(__dirname, 'src/events/'),
+        IndexedDB: path.resolve(__dirname, 'src/indexedDB/'),
+        Callbacks: path.resolve(__dirname, 'src/callbacks/'),
+        Configs: path.resolve(__dirname, 'src/configs/'),
+        Styles: path.resolve(__dirname, 'src/styles/'),
+        UI: path.resolve(__dirname, 'src/ui/'),
+        Utils: path.resolve(__dirname, 'src/utils/'),
+        Xenforo: path.resolve(__dirname, 'src/xenforo/'),
+        Visuals: path.resolve(__dirname, 'src/visuals/'),
       },
-      i18n: {
-        'ru': (headers) => ({
-          ...headers,
-          description: ru_headers['description']
-        }),
+    },
+    performance: {
+      hints: "error",
+      maxEntrypointSize: 2000 * 10 ** 3,
+      maxAssetSize: 2000 * 10 ** 3,
+    },
+    entry: path.resolve(__dirname, 'src', 'index.js'),
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: get_filename(),
+      publicPath: "/",
+    },
+    devServer: {
+      server: "http",
+      port: 11944,
+      allowedHosts: "all",
+      hot: true,
+      liveReload: false,
+      magicHtml: false,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "*",
       },
-      proxyScript: {
-        filename: "[basename].proxy.user.js",
-        baseURL: "http://localhost:11944/",
-      },
-      strict: true,
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          {
-            loader: 'style-loader',
-            options: {
-              insert: "html",
-            },
-          },
-          'css-loader',
-          'sass-loader',
-        ],
-      },
-      {
-        test: /\.(css)$/,
-        use: [
-          {
-            loader: 'style-loader',
-            options: {
-              insert: "html",
-            },
-          },
-          'css-loader',
-        ],
-      }
+      // client: {
+      //   webSocketURL: "ws://localhost:11944/ws",
+      //   progress: true,
+      //   reconnect: false
+      // },
+      client: false
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        DEV_MODE: JSON.stringify(dev)
+      }),
+      new UserscriptPlugin({
+        headers: async () => {
+          const headerPath = path.resolve(__dirname, 'src', 'headers.json');
+          const headers = JSON.parse(fs.readFileSync(headerPath).toString());
+
+          let version = headers.version;
+          let name = headers.name;
+          if (dev) {
+            headers['name'] = `[DEV] ${name}`
+            headers["version"] = `${version}-build.[buildNo]`;
+          }
+
+          return headers;
+        },
+        i18n: {
+          'ru': (headers) => ({
+            ...headers,
+            description: ru_headers['description']
+          }),
+        },
+        proxyScript: {
+          filename: "[basename].proxy.user.js",
+          baseURL: "http://localhost:11944/",
+        },
+        strict: true,
+      }),
     ],
-  },
-  optimization: {
-    emitOnErrors: true,
-    moduleIds: "named",
-    minimize: false
-  },
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            {
+              loader: 'style-loader',
+              options: {
+                insert: "html",
+              },
+            },
+            'css-loader',
+            'sass-loader',
+          ],
+        },
+        {
+          test: /\.(css)$/,
+          use: [
+            {
+              loader: 'style-loader',
+              options: {
+                insert: "html",
+              },
+            },
+            'css-loader',
+          ],
+        }
+      ],
+    },
+    optimization: {
+      emitOnErrors: true,
+      moduleIds: "named",
+      minimize: build_type === 'minify',
+    },
+  }
 }
