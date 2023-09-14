86de70e34bbb71862057d3532ce75072fcdb9dac
unknown <toil.official@yandex.com>
2023-09-15 02:28:56 +0300

    added todo comment

--- src/ui/components/icons.js
+++ src/ui/components/icons.js
@@ -1,4 +1,5 @@
 function createMenuIcon(className, id = 'LZTUpIcon') {
+  // TODO: чет с этим сделать, мне не нравится как оно выглядит
   const icon = document.createElement('i');
   icon.id = id;
   icon.className = className;
