3eb61064a94e9511ee8d3e7aeca5efb05aba9974
unknown <toil.official@yandex.com>
2023-09-21 02:30:27 +0300

    removed waiter

--- src/api/lztupgrade/loadTheme.js
+++ src/api/lztupgrade/loadTheme.js
@@ -1,16 +1,12 @@
 import endpoints from 'Configs/endpoints.json' assert {type: 'json'};
-import { waitForElement } from "Utils/utils";
 
 function loadTheme(themeName) {
+  // load theme by theme name
   const link = document.createElement('link');
   link.href = `${endpoints['staticThemes']}/${themeName}.css`;
   link.type = 'text/css';
   link.rel = 'stylesheet';
-  waitForElement('head', 3000).then(() => {
-    document
-      .querySelector('head')
-      .appendChild(link);
-  });
+  document.head.appendChild(link);
 }
 
 export { loadTheme };
