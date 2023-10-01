28a7e4f58290557ff526efc8b3936186a6279b00
unknown <toil.official@yandex.com>
2023-10-02 02:06:29 +0300

    added enum of GM Names

---
+++ src/configs/StorageName.js
@@ -0,0 +1,10 @@
+export default class StorageName {
+  static UserGroup = new StorageName('LZTUserGroup').name;
+  static Cache = new StorageName('cache').name;
+  static Contests = new StorageName('contestsData').name;
+  static Users = new StorageName('usersData').name;
+
+  constructor(name) {
+    this.name = name
+  }
+}
