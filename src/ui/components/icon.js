3b065918d5eb734d7bc293db7f68cc745e7eb9c5
unknown <toil.official@yandex.com>
2023-10-03 20:34:37 +0300

    rewrited with class

---
+++ src/ui/components/icon.js
@@ -0,0 +1,23 @@
+class Icon {
+  /**
+   *
+   *  @constructor
+   *  @param {string} className - class name of icon
+   *  @param {string} iconClassName - id of icon
+   */
+
+  constructor(className, id = 'LZTUpIcon') {
+    this.className = className;
+    this.id = id;
+  }
+
+  createElement() {
+    const icon = document.createElement('i');
+    icon.id = this.id;
+    icon.className = this.className;
+    return icon;
+  }
+}
+
+
+export default Icon;
