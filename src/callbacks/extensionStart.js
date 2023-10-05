f2ba38b9188b32fa11cd3e902c4089a36190ecac
unknown <toil.official@yandex.com>
2023-10-06 02:41:14 +0300

    getThemes replaced by themeAPI.getThemes

--- src/callbacks/extensionStart.js
+++ src/callbacks/extensionStart.js
@@ -1,11 +1,11 @@
-import { getThemes } from "API/lztupgrade/getThemes";
+import themeAPI from "API/lztupgrade/themeAPI";
 import Logger from "Utils/logger";
 
 function getThemeByID(themeId) {
   // Loading theme by ID
   return new Promise(async (resolve, reject) => {
     Logger.debug(`onExtensionStart: Start loading theme with id ${themeId}`);
-    const availabledThemes = await getThemes();
+    const availabledThemes = await themeAPI.getThemes();
     if (availabledThemes?.length) {
       Logger.debug('onExtensionStart: Themes arrray getted: ', availabledThemes);
       const findedTheme = availabledThemes.find(theme => theme.uid === themeId && theme.active === 1);
