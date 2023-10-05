2822bd1672bd1bc65a34ea044e424ec5233e0665
unknown <toil.official@yandex.com>
2023-10-06 02:47:41 +0300

    added todo comment

--- src/events/categories.js
+++ src/events/categories.js
@@ -1,5 +1,6 @@
 import { waitForElm, sleep } from 'Utils/utils';
 
+// TODO: rework with clear JS
 function onClickCategory(nodeSelector, callback) {
   const node = $(`li.node${nodeSelector}.forum.level-n`);
   $(node).on('click', async () => {
