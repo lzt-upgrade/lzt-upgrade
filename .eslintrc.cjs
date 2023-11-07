58a51c35424926a972467cf939a6d6533d1e7ecf
unknown <toil.official@yandex.com>
2023-11-07 23:32:01 +0300

    added eslint config

---
+++ .eslintrc.cjs
@@ -0,0 +1,46 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    greasemonkey: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "prettier"],
+  plugins: ["prettier"],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
+  globals: {
+    // FROM FORUM WEB
+    $: "readonly",
+    XenForo: "readonly",
+    turnstile: "readonly",
+    animateCSS: "readonly",
+    // IMPORTED JS LIBS
+    Sortable: "readonly",
+    DOMPurify: "readonly",
+    Coloris: "readonly",
+    // WEBPACK ENV
+    DEV_MODE: "readonly",
+  },
+};
