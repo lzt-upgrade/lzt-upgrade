50dfffb0733d676d00efdc8eb6304b777df3702b
unknown <toil.official@yandex.com>
2023-11-07 23:38:07 +0300

    formatted

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ import webpack from "webpack";
 import { UserscriptPlugin } from "webpack-userscript";
 import ESLintPlugin from "eslint-webpack-plugin";
 
-const dev = process.env.NODE_ENV === 'development';
+const dev = process.env.NODE_ENV === "development";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
