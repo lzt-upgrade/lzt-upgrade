d74e257120499ed5b2aebe8590204708546fa812
unknown <toil.official@yandex.com>
2023-11-10 03:00:16 +0300

    fix invalid error catching, added support request options

--- src/api/requestJSON.js
+++ src/api/requestJSON.js
@@ -1,10 +1,16 @@
-import Logger from 'Utils/logger'
+import Logger from "Utils/logger";
 
-async function requestJSON(endpoint, errText) {
+async function requestJSON(endpoint, errText, reqOptions = {}) {
   try {
-    const res = await fetch(endpoint);
+    const res = await fetch(endpoint, reqOptions).catch(err => {
+      console.error(err);
+      throw `Failed to fetch from ${endpoint}`;
+    });
 
-    return await res.json()
+    return await res.json().catch(err => {
+      console.log(err);
+      throw `Failed to parse response JSON`;
+    });
   } catch (err) {
     Logger.log(errText);
     return false;
